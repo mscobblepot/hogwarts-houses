887a8c8b31ecd0cc4e73cc5acb73172b5cea1b0a
mscobblepot <tomecsko.reka@gmail.com>
2023-02-23 19:51:04 +0100

    Create service class for Room entity

---
+++ src/main/java/com/codecool/hogwartshouses/logic/RoomService.java
@@ -0,0 +1,39 @@
+package com.codecool.hogwartshouses.logic;
+
+import com.codecool.hogwartshouses.persistence.entity.Room;
+import com.codecool.hogwartshouses.persistence.repository.RoomRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RoomService {
+    private final RoomRepository roomRepository;
+
+    public List<Room> findAll() {
+        return roomRepository.findAll();
+    }
+
+    public Optional<Room> findOne(long id) {
+        return roomRepository.findById(id);
+    }
+
+    public Room save(Room room) {
+        return roomRepository.save(room);
+    }
+
+    public void delete(long id) {
+        roomRepository.deleteById(id);
+    }
+
+    public List<Room> findAvailable() {
+        return null;
+    }
+
+    public List<Room> findAvailableForRatOwners() {
+        return null;
+    }
+}
