95a0126c5e4e1e54052ba06399d6b5a0baab9980
mscobblepot <tomecsko.reka@gmail.com>
2023-02-24 15:53:15 +0100

    Create tests for RoomEndpoint getOne() method

--- src/test/java/com/codecool/hogwartshouses/api/endpoint/RoomEndpointTest.java
+++ src/test/java/com/codecool/hogwartshouses/api/endpoint/RoomEndpointTest.java
@@ -1,13 +1,17 @@
 package com.codecool.hogwartshouses.api.endpoint;
 
 import com.codecool.hogwartshouses.logic.RoomService;
+import com.codecool.hogwartshouses.persistence.entity.Room;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -28,6 +32,30 @@ class RoomEndpointTest {
                 .andExpect(status().isOk());
 
         verify(roomService).findAll();
+    }
+
+    @Test
+    void getOneFindsEntity() throws Exception {
+        long id = 1;
+        String uri = url + "/" + id;
+        Room room = Room.builder().id(id).build();
+        when(roomService.findOne(id)).thenReturn(Optional.of(room));
+
+        mockMvc.perform(get(uri))
+                .andExpect(status().isOk());
+
+        verify(roomService).findOne(id);
+    }
+
+    @Test
+    void getOneDoesNotFindEntity() throws Exception {
+        long id = 1;
+        String uri = url + "/" + id;
+
+        mockMvc.perform(get(uri))
+                .andExpect(status().is4xxClientError());
+
+        verify(roomService).findOne(id);
     }
 
 }
